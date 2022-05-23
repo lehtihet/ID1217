a487f9078e1f401ac209e6ea9cba354451e107aa
lehtihet <60138988+lehtihet@users.noreply.github.com>
2022-05-23 06:08:00 +0200

    Add files via upload

--- HW4/RollerCoaster.java
+++ HW4/RollerCoaster.java
@@ -1,27 +1,115 @@
+/*
+*
+* Test: 
+* 	java RollerCoaster 2 2 2 5000
+* Should give :
+*	Car starts to load
+*	Passenger 0 and 1 take ride
+*	Car starts around the track
+*	(wait 5 seconds)
+*	Car unloads
+*	Passenger 0 and 1 return from ride
+*	Car starts to load
+*	Passenger 0 and 1 take ride
+*	Car starts around the track
+* 	(wait 5 seconds)
+*	Car unloads
+*	Passenger 0 and 1 return from ride
+*	All passengers are done
+*
+* Test 2:
+*	java RollerCoaster 10 20 2 2000
+*/
 
+
+import java.util.Random;
+import java.util.ArrayList;
+
+class Shared {
+	static long startT = System.currentTimeMillis();
+
+	public static int timeStamp() {
+		return (int) (System.currentTimeMillis() - startT)/1000;
+	}
+}
 
 class Passenger extends Thread {
 
-}
-
-class Car extends Thread {
-
-	int passengers;
-	int C;
+	int pid;
+	int numRides;
+	Random r;
 	Monitor m;
 
-	public Car(int capacity, Monitor m) {
-		this.c = capacity;
+	public Passenger(int pid, int numRides, Random r, Monitor m) {
+		this.pid = pid;
+		this.numRides = numRides;	
+		this.r = r;
 		this.m = m;
 	}
 
 	@Override
 	public void run() {
+		
+		for (int i = 0; i < numRides; i++) {
+			// Do stuff
+			try {
+				Thread.sleep(2000+r.nextInt(1000));
+			} catch (Exception e) {}
 
-		m.load(c);
+			System.out.printf("%d: Passenger %d takes ride\n", Shared.timeStamp(), pid);
+			m.takeRide();
+			System.out.printf("%d: Passenger %d returns from ride\n", Shared.timeStamp(), pid);
+		}
 
-		m.unload();
 
+	}
+
+}
+
+class Car extends Thread {
+
+	int rideTime;
+	int c;
+	Monitor m;
+	int run;
+
+	public Car(int rideTime, int capacity, Monitor m) {
+		this.rideTime = rideTime;
+		this.c = capacity;
+		this.m = m;
+		this.run = 1;
+	}
+
+	@Override
+	public void run() {
+
+		while(run == 1) {
+
+			System.out.printf("%d: Car starts to load\n", Shared.timeStamp());
+
+			m.load(c);
+
+			System.out.printf("%d: Car starts around the track\n", Shared.timeStamp());
+
+			// Ready to go
+
+			try {
+				Thread.sleep(rideTime);
+			} catch (Exception e) {}
+
+			System.out.printf("%d: Car unloads\n", Shared.timeStamp());
+			m.unload();
+
+			try {
+				Thread.sleep(2000);
+			} catch (Exception e) {}
+
+		}
+
+	}
+
+	public void kill() {
+		this.run = 0;
 	}
 
 }
@@ -40,21 +128,23 @@ class Monitor {
 
 	public synchronized void load(int c) {
 
+		// Another solution: have each passenger decrease waitingP by 1 and wait here untill
+		// waitingP == 0 then change takeRide to 0
+		
 		takenRide = 0;
-		notifyAll() // Wake those sleeping in round two at beginning of takeRide()
+		notifyAll(); // Wake those sleeping in round two at beginning of takeRide()
 
 		try {
 			while(waitingPassengers < this.c) wait();
 		} catch (Exception e) {}
-
-		// Ready to go
-		Thread.sleep(500);
 		
 
 	}
 
 	public synchronized void unload() {
+
 		takenRide = 1;
+		waitingPassengers -= c;
 		notifyAll();
 
 	}
@@ -62,7 +152,7 @@ class Monitor {
 	public synchronized void takeRide() {
 
 		try {
-			while (waitingPassengers >= this.c || takenRide == 1) wait();
+			while (waitingPassengers >= this.c || takenRide == 1) wait(); 
 		} catch (Exception e) {}
 
 
@@ -71,9 +161,7 @@ class Monitor {
 
 		try {
 			while(takenRide != 1) wait();
-		} Catch (Exception e) {}
-
-		waitingPassengers--;
+		} catch (Exception e) {}
 
 
 	}
@@ -87,19 +175,38 @@ public class RollerCoaster {
 	public static void main (String[] args) {
 
 
+		int c = Integer.parseInt(args[0]);
+		int n = Integer.parseInt(args[1]);
+		int numRides = Integer.parseInt(args[2]);
+		int rideTime = Integer.parseInt(args[3]);
+
+		Random rand = new Random();
+		Monitor m = new Monitor(c);
+
+		ArrayList<Passenger> passengerList = new ArrayList<Passenger>();
+
+		Car carThread = new Car(rideTime, c, m);
+
+		carThread.start();
+
+		for (int i = 0; i < n; i++) {
+			passengerList.add(new Passenger(i, numRides, rand, m));
+			passengerList.get(i).start();
+		}
+
+
+		for (Passenger p : passengerList) {
+			try {
+				p.join();
+			} catch (Exception e) {}
+		}
+
+		System.out.println(Shared.timeStamp() + ": All passengers done!");
+
+		carThread.kill();
+
 
 	}
 
 }
 
-/*
-* Monitor: Varje passagerare kör takeRide vilket i monitorn kallar på wait() 
-* tills takenRide = 1 och ökar passengers i monitorn
-* Car kallar load vilket väntar tills passengers=C och som sedan väntar rideTime och 
-* sätter takenRide = 1. Efter ett tag sätter car passengers = takenRide = 0 (kanske i unload?).
-*
-*
-*
-*
-*
-*/
